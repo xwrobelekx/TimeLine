45c3606ef5c88ac497b8d3cff60904c1fb45676f
Kamil Wrobel <wrobelk@live.com>
2018-09-25 19:08:40 -0600

    created DetailTableVeiw UI, connets the outlets and actions, creates alert Controller

--- TimeLine/ViewControllers/PostDetailTableViewController.swift
+++ TimeLine/ViewControllers/PostDetailTableViewController.swift
@@ -13,45 +13,71 @@ class PostDetailTableViewController: UITableViewController {
     //MARK: - Properties
     var post : Post?{
         didSet{
+            
             updateViews()
         }
     }
     
     //MARK: - Outlets
     
+    @IBOutlet weak var photoImageView: UIImageView!
     
-    
-    //MARK: - Lifecycle Methods
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-
-    }
-    
-    
-    
-    
-    
-
-    // MARK: - Table view data source
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
-    }
-
-  
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "xxxxxxxx", for: indexPath)
-
+        //FIXME: were supose to implement row height here, but tableView had automatic dimension by default
        
-
-        return cell
     }
 
+    
+    
+    //MARK: - Actions
+    
+    @IBAction func commentButtonTapped(_ sender: Any) {
+        //UIAlertController with textField - cancel & ok - ok actions inits new comment via PostController - reloads tableView to display it - do not create new comment if user did add any text
+        addComentAlert()
+    }
+    
+    @IBAction func followButtonTapped(_ sender: Any) {
+    }
+    
+    @IBAction func shareButtonTapped(_ sender: Any) {
+    }
+    
+    
+    
     //MARK: - Helper Methods
     func updateViews() {
+        guard let post = post else {return}
+        photoImageView.image = post.photo ?? UIImage(named: "slc")
         
         //FIXME: update Labels
+        //FIXME: relod the table view if needed
     }
-
-
+    
+    func addComentAlert() {
+        let alert = UIAlertController(title: "Add Comment:", message: nil, preferredStyle: .alert)
+        alert.addTextField { (commentTextField) in
+            commentTextField.placeholder = "Enter coment here..."
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (commentCreated) in
+            
+           guard let commentTextField = alert.textFields?.first,
+            let comment = commentTextField.text, comment != "" else {return}
+            guard let post = self.post else {return}
+            PostController.shared.addComment(text: comment, post: post , completion: { (xxxx) in
+                //FIXME: completion may be needed
+            })
+            
+        }))
+        
+        
+        
+        
+    }
+    
+    
+    
 }
